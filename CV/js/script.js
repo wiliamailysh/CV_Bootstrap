f26931711373f85e3f927a5c6bab8e5df6ff0457
wiliamailysh <victor.douay+dev@gmail.com>
2019-05-31 13:00:59 +0200

    Progress

---
+++ CV/js/script.js
@@ -0,0 +1,10 @@
+// window.addEventListener('resize', function() {
+//   let mobile = window.innerWidth < 992;
+//   if (mobile) {
+//     document.querySelector('.timeline__mobile').style.display = 'block';
+//     document.querySelector('.timeline__desktop').style.display = 'none';
+//   } else {
+//     document.querySelector('.timeline__desktop').style.display = 'block';
+//     document.querySelector('.timeline__mobile').style.display = 'none';
+//   }
+// })
